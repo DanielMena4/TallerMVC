9dc9bc3e17305e188cd2e7a19047c9a4a94fb086
DanielMena4 <daniel__mena@hotmail.com>
2025-04-07 08:50:18 -0500

    Creacion de vista Equipo

---
+++ TallerMVC/Controllers/EquipoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TallerMVC.Models;
+using TallerMVC.Repositories;
+
+namespace TallerMVC.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public ActionResult View()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            EquipoRepository equipoRepository = new EquipoRepository();
+            var equipos = equipoRepository.DevuelveListdoEquipos();
+            return View(equipos);
+        }
+
+    }
+}
---
+++ TallerMVC/Repositories/EquipoRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using TallerMVC.Models;
+
+namespace TallerMVC.Repositories
+{
+    public class EquipoRepository
+    {
+        public IEnumerable<Equipo> DevuelveListdoEquipos()
+        {
+
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                Id = 1,
+                Nombre = "Liga de Quito",
+                PartidosJugados = 10,
+                PartidosGanados = 10,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0,
+            };
+            equipos.Add(ldu);
+            return equipos;
+        }
+    }
+}
